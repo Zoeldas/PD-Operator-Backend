e09bb2c03c2ed720a9028d0f0b702bc000516533
Zoeldas <71159094+Zoeldas@users.noreply.github.com>
2022-10-29 23:49:58 -0700

    Add files via upload
    no front end test

---
+++ AuthenticateUser/src/main/java/Dao/UserDao.java
@@ -0,0 +1,6 @@
+package Dao;
+import Entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserDao extends CrudRepository<User,String> {
+}
---
+++ AuthenticateUser/src/main/java/Service/UserService.java
@@ -0,0 +1,12 @@
+package Service;
+
+import Entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserService {
+
+    public User getUser(User user);
+
+    public User insertIntoDatabase(User user);
+}
